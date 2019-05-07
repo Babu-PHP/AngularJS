90c45f51525b8aec8b0edfc06ad932a717bc014b
Babu-PHP <bheema.babi@gmail.com>
2019-05-07 16:41:13 +0530

    Added updated files

---
+++ checkuser.php
@@ -0,0 +1,49 @@
+<?php
+ 
+ include "includes/pdodbconnection.php";
+
+$post_date = file_get_contents("php://input");
+$data = json_decode($post_date);
+ 
+ 
+//Here you can write your SQL queries to save the data into databases.
+ 
+//I will just show form values here using echo which we submit using AngularJS
+//echo "Name : ".$data->name."\n";
+//echo "Email : ".$data->email."\n";
+//echo "Username : ".$data->username."\n";
+//echo "Password : ".$data->password."\n";
+ 
+$status = 'A';
+ /* $sql_insert = $db->prepare("INSERT INTO users(name, emailid, username, password, status) VALUES (:name, :emailid, :username, :password, :status)");
+        $sql_insert->bindParam(':name',$data->name);
+        $sql_insert->bindParam(':emailid',$data->email);
+        $sql_insert->bindParam(':username',$data->username);
+        $sql_insert->bindParam(':username',$data->password);
+        $sql_insert->bindParam(':status',$status); */
+
+        $sth = $db->prepare('SELECT name, colour, calories FROM users WHERE username = :username AND password = :colour');
+$sth->bindParam(':username', $data->username, PDO::PARAM_STR, 12);
+$sth->bindParam(':password', $data->password, PDO::PARAM_STR, 12);
+//$sth->execute();
+
+
+//print_r($sql_insert); 
+//$sq = $sql_insert->debugDumpParams();
+//$sq = $sql_insert->getSQL() . PHP_EOL;
+
+$json_sql_insert = json_encode($sq);//json_encode($sql_insert);
+       //$this->Print_log("sql insert: ".$json_sql_insert, "Reserve Api");
+//print_r($sql_insert);
+        $return_msg=0;
+    try {
+        if($sth->execute()){
+        	$return_msg=1;
+        }
+    } catch(Exception $e){
+        echo $e->getMessage();
+    }
+
+        echo $return_msg;//$data->name;//$json_sql_insert;
+ 
+?>
--- login (copy).php
+++ login (copy).php
@@ -124,11 +124,8 @@ span.psw {
     }
 }
 </style>
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 </head>
-<body ng-app="myApp" ng-controller="formCtrl">
+<body>
 
 <h2>Modal Login Form</h2>
 
@@ -136,7 +133,7 @@ span.psw {
 
 <div id="id01" class="modal">
   
-  <form class="modal-content animate" name="myForm" novalidate ng-submit="myFunc()">
+  <form class="modal-content animate" action="/action_page.php">
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
       <img src="img_avatar2.png" alt="Avatar" class="avatar">
@@ -144,21 +141,12 @@ span.psw {
 
     <div class="container">
       <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="username" ng-model="username" required>
-      <span style="color:red" ng-show="myForm.username.$dirty && myForm.username.$invalid">
-      <span ng-show="myForm.username.$error.required">Username is required.</span>
-      </span>
+      <input type="text" placeholder="Enter Username" name="uname" required>
 
       <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="password" ng-model="password" required>
-      <span style="color:red" ng-show="myForm.password.$dirty && myForm.password.$invalid">
-      <span ng-show="myForm.password.$error.required">Password is required.</span>
-      </span>
-    
-    <input type="submit"
-      ng-disabled="myForm.username.$dirty && myForm.username.$invalid ||  
-      myForm.email.$dirty && myForm.email.$invalid" ng-click="formsubmit(myForm.$valid)">
-      <!-- <button type="submit">Login</button> -->
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
@@ -169,11 +157,6 @@ span.psw {
       <span class="psw">Forgot <a href="#">password?</a></span>
     </div>
   </form>
-  <p>
-    <pre ng-model="result">
-        {{result}}
-    </pre>
- </p>
 </div>
 
 <script>
@@ -187,54 +170,6 @@ window.onclick = function(event) {
     }
 }
 </script>
-
-<script>
-var app = angular.module('myApp', []);
-/* app.controller('formCtrl', function($scope) {
-    //$scope.name = "John";
-    //$scope.user = 'John Doe';
-    //$scope.email = 'John.Doe@gmail.com';
-    $scope.myTxt = "You have not yet clicked submit";
-    $scope.myFunc = function () {
-        $scope.myTxt = "You clicked submit!";
-    }
-}); */
-
-app.controller("formCtrl", ['$scope', '$http', function($scope, $http) {
-   var vm = this; // self-referencing local variable required for when promise resolves
-    vm.model = {};
-        $scope.url = 'checkuser.php';
- 
-        $scope.formsubmit = function(isValid) {
- 
- 
-            if (isValid) {              
- 
-                $http.post($scope.url, {"name": $scope.name, "email": $scope.email, "username": $scope.username, "password": $scope.password}).
-                        then(function(data, status) {
-                            console.log(data);
-                            $scope.status = status;
-                            $scope.data = data;
-                            $scope.result = data; // Show result from server in our <pre></pre> element
-
-                            vm.model = {}; // this resets the model
-                            vm.myForm.$setPristine(); // this resets the form itself
-
-                        })
-            }else{
-                
-                  alert('Form is not valid');
-            }
- 
- 
-        }
- 
- 
- 
- 
-    }]);
-</script>
-
 
 </body>
 </html>
